b0e96886c73ed7acd6f878b3d4bb51e4e5b96e6b
Nhật <vvkghouls@gmail.com>
2025-03-19 13:04:41 +0700

    Quên import react vào app.jsx

--- faceID-frontend/src/App.jsx
+++ faceID-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./AuthContext";
 import Dashboard from "./components/Dashboard/Dashboard";
