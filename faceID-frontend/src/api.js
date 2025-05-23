ac876f74a42e3a66f0b69890bd2f46d1c95e91e3
mkhoi2004 <118917037+mkhoi2004@users.noreply.github.com>
2025-05-24 00:27:56 +0700

    xoa bo local o file api.js

--- faceID-frontend/src/api.js
+++ faceID-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 console.log('Connecting to API at:', API_URL);
 
 // Tạo axios instance với baseURL
